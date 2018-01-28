3a9f925201fcf155bfce825efeac2b32677e6a6a
David <dmn@Davids-MacBook-Pro.local>
2018-01-28 16:22:39 -0500

    updates for week1

--- 001/src/main/scala/Main.scala
+++
@@ -1,7 +0,0 @@
-// import example.Lists._
-object Main extends App {
-    println("Max")
-    println(example.Lists.max(List(1,3,2)))
-    println("Sum")
-    println(example.Lists.sum(List(1,3,2)))
-}
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,5 @@
+// import example.Lists._
+object Main extends App {
+    println("Sqrt")
+    println( session.Session.sqrt(1e-6) )
+}
