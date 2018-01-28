445e3d86462318e1b45be24c64119352b2620047
David <dmn@Davids-MacBook-Pro.local>
2018-01-28 13:18:47 -0500

    initial

---
+++ 001/src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+// import example.Lists._
+object Main extends App {
+    println("Max")
+    println(example.Lists.max(List(1,3,2)))
+    println("Sum")
+    println(example.Lists.sum(List(1,3,2)))
+}
